af0ad400a3798c46edce91bb921d64396e008017
Renan Pallin <renanpallin@gmail.com>
2017-08-18 21:07:40 -0300

    export controller to a separate file

--- src/main/java/com/test/campaingapi/CampaignApiApplication.java
+++ src/main/java/com/test/campaingapi/CampaignApiApplication.java
@@ -2,35 +2,14 @@ package com.test.campaingapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.test.campaingapi.model.Campaign;
-import com.test.campaingapi.model.Team;
 
 @RestController
 @SpringBootApplication
 public class CampaignApiApplication {
 
-	@RequestMapping("/")
-	public String hello() {
-		return "hello!!!";
-	}
-	
-	@RequestMapping("/campaign")
-	Campaign campaign() {
-		Campaign campaign = new Campaign();
-		campaign.setId(564);
-		campaign.setNome("fuuck");
-		
-		Team team = new Team();
-		team.setNome("Curintia");
-		campaign.setTeam(team);
-		
-		return campaign;
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(CampaignApiApplication.class, args);
 	}
+	
 }
---
+++ src/main/java/com/test/campaingapi/CampaignController.java
@@ -0,0 +1,25 @@
+package com.test.campaingapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.campaingapi.model.Campaign;
+import com.test.campaingapi.model.Team;
+
+@RestController
+@RequestMapping("/campaign")
+public class CampaignController {
+
+	@RequestMapping("")
+	Campaign campaign() {
+		Campaign campaign = new Campaign();
+		campaign.setId(564);
+		campaign.setNome("Campanha linda do contorler");
+		
+		Team team = new Team();
+		team.setNome("Curintia");
+		campaign.setTeam(team);
+		
+		return campaign;
+	}
+}
